738a0daa68663ea42d3cef234b66eca43166cc96
Nicolas LEROY <leroy@macbook-leroy.ipgp.fr>
2016-05-20 15:13:10 +0200

    initial commit

---
+++ dataless_modify.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 13 14:56:35 2016
+
+@author: leroy
+"""
+
+from obspy import UTCDateTime
+from obspy.io.xseed import Parser
+import copy
+import sys
+
+# Load Dataless to modify, assume only one station
+p = Parser("dataless.G.CAY.seed")
+
+# Get Encoding format
+if p.abbreviations[0].blockette_type == 30:
+    if p.abbreviations[0].data_family_type == 1:
+        # Test if encoding is Geoscope 3 bits
+        if p.abbreviations[0].short_descriptive_name.rfind("3") != -1:
+            # Set gain to 2^7 = 128
+            gain = 2**7
+        # Or if encoding is Geoscope 4 bits
+        elif p.abbreviations[0].short_descriptive_name.rfind("4") != -1:
+            # Set gain to 2^15 = 32768
+            gain = 2**15
+        else :
+            # Print warning and exit
+            print("No Geoscope encoding")
+            sys.exit()
+                
+        
+
+# Get Station = first station
+blksta = p.stations[0]
+
+# Iterate to find all stages 0 and insert a new blockette 58 with gain
+# before + update gain on stage 0
+# --------------------------------------------------------------------
+
+# Start from 1 to skip blockette 50
+i=1
+while i < len(blksta):
+    # Test if blockette is a gain blockette
+    if blksta[i].blockette_type == 58:   
+        # Test if the blockette is the last of the channel = stage 0
+        if blksta[i].stage_sequence_number == 0:
+            # Update gain on stage 0
+            blksta[i].sensitivity_gain *= gain
+            # Insert a copy of gain of the last stage
+            blksta.insert(i, copy.deepcopy(blksta[i-1]))
+            # Update gain on new stage
+            blksta[i].sensitivity_gain = gain
+            # Increment stage number on new blockette
+            blksta[i].stage_sequence_number = blksta[i].stage_sequence_number + 1
+            # Skip next blockette (= stage 0 that has been shifted due to insert)            
+            i+=1
+    # Go to next blockette
+    i+=1
+
+
+# Update information for new encoding
+# -----------------------------------
+
+# Get a blockette 30 with Steim2 encoding
+psteim2 = Parser("dataless.G.CLF.seed")
+# Copy it on current dataless
+p.abbreviations[0] = psteim2.abbreviations[0]
+
+        
+# Write Dataless    
+p.write_seed("CAY.dataless.modif")
+
